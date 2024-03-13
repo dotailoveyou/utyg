428ff0a65c356d00ddbf1336613fbb179e4963af
dotailoveyou <156932966+dotailoveyou@users.noreply.github.com>
2024-03-13 19:50:07 +0300

    скуфн
    зшгргщрш

--- qwertyh.py
+++ qwertyh.py
@@ -1,16 +1,41 @@
+import pygame
+pygame.init()
+pygame.display.set_mode()
 
-class Button:
-    a = 0
-    def click(self):
-        self.a = self.a + 1
+class Circle:
+    def __init__(self,radius,x, y, surface, color):
+        self.surface = surface
+        self.color = color
+        self.x = x
+        self.y = y
+        self.radius =  radius
 
-    def click_count(self):
-        print(self.a)
+    def draw(self):
+        pygame.draw.circle(self.surface, self.color, (self.x,self.y), self.radius)
 
-    def reset(self):
-        self.a = 0
+   def move_by_keys(self):
+       keys = pygame.key.get_pressed()
+       if keys[pygame.K_LEFT]:
+           self.x -= 3
+       elif keys[pygame.K_RIGHT]:
+           self.x += 3
+       elif keys[pygame.K_UP]:
+           self.y -= 3
+       elif keys[pygame.K_DOWN]:
+           self.y += 3
 
-bu = Button()
-bu.click()
-bu.click_count()
-bu.reset()
+
+##создать объект
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+
+    win.fill((0, 225, 225))
+    #вызываем методы
+
+
+    pygame.time.delay(10)
+    pygame.display.update()
